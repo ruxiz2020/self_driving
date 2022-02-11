7f8e562e62320a6ebdc790c19943bd2b58cfe638
ruxiz2020 <zruxi@vmware.com>
2022-02-11 13:23:19 -0800

    update

--- server/lab1_mapping.py
+++ server/lab1_mapping.py
@@ -37,13 +37,13 @@ def mapping():
                 if (i < 90) & (data_dist < 15): # obstacle on the left
                     PWM.setMotorModel(1500,1500,-1500,-1500) # turn right
                     time.sleep(1)
-                    PWM.setMotorModel(-1500,-1500,1500,1500) # turn Left
+                    #PWM.setMotorModel(-1500,-1500,1500,1500) # turn Left
                     #print("STOPPING!")
                     #PWM.setMotorModel(0,0,0,0)
                 elif (i >= 90) & (data_dist < 15): # obstacle on the left
                     PWM.setMotorModel(-1500,-1500,1500,1500) # turn Left
                     time.sleep(1)
-                    PWM.setMotorModel(1500,1500,-1500,-1500) # turn right
+                    #PWM.setMotorModel(1500,1500,-1500,-1500) # turn right
                 else:
                     PWM.setMotorModel(400,400,400,400) #Forward
                     print ("The car is moving forward")
