485823750a125bdcc53a3bc0e6ab90299c7c8818
ruxiz2020 <zruxi@vmware.com>
2022-02-11 10:02:25 -0800

    update

--- server/lab1_mapping.py
+++ server/lab1_mapping.py
@@ -18,10 +18,10 @@ adc = Adc()
 buzzer = Buzzer()
 
 
-def move_or_stop_according_to_detected_distance(distince_input, PWM):
+def move_or_stop_according_to_detected_distance(degree, distince_input, PWM):
 
     data=ultrasonic.get_distance()   #Get the distance value
-    print ("When servo is at "+str(i)+" degree")
+    print ("When servo is at "+str(degree)+" degree")
     print ("Obstacle distance is "+str(data)+" CM")
 
     if data < distince_input:
@@ -42,12 +42,12 @@ def mapping(distince_input):
         for i in range(40, 160, 1):
             pwm.setServoPwm('0', i)
             time.sleep(0.01)
-            move_or_stop_according_to_detected_distance(D, PWM)
+            move_or_stop_according_to_detected_distance(i, D, PWM)
 
         for i in range(160,40,-1):
             pwm.setServoPwm('0',i)
             time.sleep(0.01)
-            move_or_stop_according_to_detected_distance(D, PWM)
+            move_or_stop_according_to_detected_distance(i, D, PWM)
 
     #except KeyboardInterrupt:
     #    PWM.setMotorModel(0,0,0,0)
