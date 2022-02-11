6356159cad7679aef8c4739a6a323962e5a45e9c
ruxiz2020 <zruxi@vmware.com>
2022-02-11 12:18:26 -0800

    update

--- server/lab1_mapping.py
+++ server/lab1_mapping.py
@@ -11,6 +11,7 @@ from Thread import *
 
 
 led = Led()
+PWM = Motor()
 ultrasonic = Ultrasonic()
 line = Line_Tracking()
 pwm = Servo()
@@ -18,47 +19,42 @@ adc = Adc()
 buzzer = Buzzer()
 
 
-def move_or_stop_according_to_detected_distance(degree, distince_input, PWM):
+def move_or_stop_according_to_detected_distance(distince_input):
 
     data=ultrasonic.get_distance()   #Get the distance value
-    print ("When servo is at "+str(degree)+" degree")
+    print ("When servo is at "+str(i)+" degree")
     print ("Obstacle distance is "+str(data)+" CM")
 
     if data < distince_input:
         print("STOPPING!")
-        #pwm.setServoPwm('0', 90)
-        #time.sleep(0.01)
         PWM.setMotorModel(0,0,0,0)
     else:
         PWM.setMotorModel(500,500,500,500) #Forward
         print ("The car is moving forward")
 
-    return (degree, data)
-
 
 def mapping(distince_input):
 
-    PWM = Motor()
-
     D = int(distince_input) # distince threshold for stopping
 
-    while True:
-        array_distances = []
-        for i in range(40, 160, 1):
-            pwm.setServoPwm('0', i)
-            time.sleep(0.01)
+    try:
+        while True:
+            for i in range(40, 160, 1):
+                pwm.setServoPwm('0', i)
+                time.sleep(0.01)
+                move_or_stop_according_to_detected_distance(distince_input)
 
-            (degree, dist) = move_or_stop_according_to_detected_distance(i, D, PWM)
-            array_distances.append(degree, dist)
+            for i in range(160,40,-1):
+                pwm.setServoPwm('0',i)
+                time.sleep(0.01)
+                move_or_stop_according_to_detected_distance(distince_input)
 
-        for i in range(160, 40, -1):
-            pwm.setServoPwm('0', degree)
-            time.sleep(0.01)
 
-            (degree, dist) = move_or_stop_according_to_detected_distance(i, D, PWM)
-            array_distances.append(degree, dist)
-        print(array_distances)
 
+
+    except KeyboardInterrupt:
+        PWM.setMotorModel(0,0,0,0)
+        print ("\nEnd of program")
 
 
 # Main program logic follows:
@@ -71,5 +67,4 @@ if __name__ == '__main__':
         dist = sys.argv[1]
         mapping(dist)
     except:
-        print(sys.argv[1])
         print("Please input a distinct theshold for stopping!")
