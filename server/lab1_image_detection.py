dff744846cbce7c8d29ac1d339889bbf7e31ce6b
ruxiz2020 <zruxi@vmware.com>
2022-02-12 08:46:35 -0800

    img detection

--- server/lab1_image_detection.py
+++ server/lab1_image_detection.py
@@ -12,6 +12,7 @@ from Thread import *
 from picamera import PiCamera
 from time import sleep
 camera = PiCamera()
+from detect import *
 
 
 
@@ -33,19 +34,16 @@ def run():
                 pwm.setServoPwm('0', i)
                 time.sleep(0.01)
 
-                data_dist=ultrasonic.get_distance()   #Get the distance value
-                #print ("When servo is at "+str(i)+" degree")
-                #print ("Obstacle distance is "+str(data)+" CM")
-                arr_dist.append((i, data_dist))
-                print(arr_dist)
-
                 PWM.setMotorModel(400,400,400,400) #Forward
                 print ("The car is moving forward")
 
-                if (i > 70) & (i < 110) & (data_dist < 15): # obstacle on the left
-                    PWM.setMotorModel(0,0,0,0)
+                data_dist=ultrasonic.get_distance()   #Get the distance value
+                if (data_dist < 20):
+                    PWM.setMotorModel(0,0,0,0) # stop
                     time.sleep(1)
-                    camera.capture('sign.jpg')
+                    #camera.capture('sign.jpg') # take picture
+                    detections = run_detection('efficientdet_lite0.tflite') # object detection
+                    print(detections)
 
                 else:
                     PWM.setMotorModel(400,400,400,400) #Forward
