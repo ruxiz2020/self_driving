d4a710087e5b12c6a902e4ca7eb6f0640d7bb3f8
ruxiz2020 <zruxi@vmware.com>
2022-05-14 21:16:06 -0700

    modify servo move

--- chatbot_car/servo.py
+++ chatbot_car/servo.py
@@ -1,52 +1,57 @@
 import time
 from PCA9685 import PCA9685
+
+
 class Servo:
     def __init__(self):
         self.PwmServo = PCA9685(0x40, debug=True)
         self.PwmServo.setPWMFreq(50)
-        self.PwmServo.setServoPulse(8,1500)
-        self.PwmServo.setServoPulse(9,1500)
-    def setServoPwm(self,channel,angle,error=10):
-        angle=int(angle)
-        if channel=='0':
-            self.PwmServo.setServoPulse(8,2500-int((angle+error)/0.09))
-        elif channel=='1':
-            self.PwmServo.setServoPulse(9,500+int((angle+error)/0.09))
-        elif channel=='2':
-            self.PwmServo.setServoPulse(10,500+int((angle+error)/0.09))
-        elif channel=='3':
-            self.PwmServo.setServoPulse(11,500+int((angle+error)/0.09))
-        elif channel=='4':
-            self.PwmServo.setServoPulse(12,500+int((angle+error)/0.09))
-        elif channel=='5':
-            self.PwmServo.setServoPulse(13,500+int((angle+error)/0.09))
-        elif channel=='6':
-            self.PwmServo.setServoPulse(14,500+int((angle+error)/0.09))
-        elif channel=='7':
-            self.PwmServo.setServoPulse(15,500+int((angle+error)/0.09))
+        self.PwmServo.setServoPulse(8, 1500)
+        self.PwmServo.setServoPulse(9, 1500)
+
+    def setServoPwm(self, channel, angle, error=10):
+        angle = int(angle)
+        if channel == '0':
+            self.PwmServo.setServoPulse(8, 2500 - int((angle + error) / 0.09))
+        elif channel == '1':
+            self.PwmServo.setServoPulse(9, 500 + int((angle + error) / 0.09))
+        elif channel == '2':
+            self.PwmServo.setServoPulse(10, 500 + int((angle + error) / 0.09))
+        elif channel == '3':
+            self.PwmServo.setServoPulse(11, 500 + int((angle + error) / 0.09))
+        elif channel == '4':
+            self.PwmServo.setServoPulse(12, 500 + int((angle + error) / 0.09))
+        elif channel == '5':
+            self.PwmServo.setServoPulse(13, 500 + int((angle + error) / 0.09))
+        elif channel == '6':
+            self.PwmServo.setServoPulse(14, 500 + int((angle + error) / 0.09))
+        elif channel == '7':
+            self.PwmServo.setServoPulse(15, 500 + int((angle + error) / 0.09))
 
 
-pwm=Servo()
+pwm = Servo()
+
+
 def test_Servo():
     try:
         while True:
-            for i in range(50,110,1):
-                pwm.setServoPwm('0',i)
-                time.sleep(0.01)
-            for i in range(110,50,-1):
-                pwm.setServoPwm('0',i)
-                time.sleep(0.01)
-            for i in range(80,150,1):
-                pwm.setServoPwm('1',i)
-                time.sleep(0.01)
-            for i in range(150,80,-1):
-                pwm.setServoPwm('1',i)
-                time.sleep(0.01)
+            for i in range(50, 110, 1):
+                pwm.setServoPwm('0', i)
+                time.sleep(0.1)
+            for i in range(110, 50, -1):
+                pwm.setServoPwm('0', i)
+                time.sleep(0.1)
+            for i in range(80, 150, 1):
+                pwm.setServoPwm('1', i)
+                time.sleep(0.1)
+            for i in range(150, 80, -1):
+                pwm.setServoPwm('1', i)
+                time.sleep(0.1)
     except KeyboardInterrupt:
-        pwm.setServoPwm('0',90)
-        pwm.setServoPwm('1',90)
-        print ("\nEnd of program")
+        pwm.setServoPwm('0', 90)
+        pwm.setServoPwm('1', 90)
+        print("\nEnd of program")
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     test_Servo()
